c532e6721a9b75718101605de58b8e610c7e63b4
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2024-03-18 20:07:39 +0800

    Formatted files with intelephense

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="icon" href="https://www.tarc.edu.my/images/tarIco.ico" />
-		<title>TAR UMT Robotics Club</title>
 
-		<link rel="stylesheet" href="css/reset.css" />
-		<link rel="stylesheet" href="css/style.css" />
-		<link rel="stylesheet" href="css/navbar.css" />
-		<link rel="stylesheet" href="css/index.css" />
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" href="https://www.tarc.edu.my/images/tarIco.ico" />
+	<title>TAR UMT Robotics Club</title>
 
-		<!-- Font Awesome -->
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	</head>
-	<body>
-		<?php
-            require_once('includes/navbar.php');
-        ?>
+	<link rel="stylesheet" href="css/reset.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/navbar.css" />
+	<link rel="stylesheet" href="css/index.css" />
 
-		<section class="hero-section">
-			<div class="hero-container">
-				<div class="text-box">
-					<h1>Welcome to TAR UMT Robotics Club</h1>
-					<p>Explore the world of robotics with us and unleash your creativity!</p>
-				</div>
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+	<?php
+	require_once('includes/navbar.php');
+	?>
+
+	<section class="hero-section">
+		<div class="hero-container">
+			<div class="text-box">
+				<h1>Welcome to TAR UMT Robotics Club</h1>
+				<p>Explore the world of robotics with us and unleash your creativity!</p>
 			</div>
-		</section>
-	</body>
+		</div>
+	</section>
+</body>
+
 </html>
