15d7bc911430b29b0950699375b6fa0a48730a2f
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2024-03-26 11:42:52 +0800

    Edit admin panel

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@ include('includes/header-admin.php');
         <h2>Welcome <b><?php echo ADMIN_USER ?></b>, what would you like to do?</h3>
 
             <div class="grid">
-                <a href=""><i class="fa-solid fa-list-check"></i>Manage Projects</a>
+                <a href=""><i class="fa-regular fa-calendar-days"></i>Manage Events</a>
                 <a href="add-news.php"><i class="fa-solid fa-newspaper"></i> Manage News</a>
                 <a href="users.php"><i class="fa-solid fa-users"></i>Manage Users</a>
                 <a href=""><i class="fa-solid fa-bell"></i>Manage Notification</a>
