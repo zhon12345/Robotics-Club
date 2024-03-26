52c55a65c61df00d1d24216ccf94b3283653b849
Zhxua <163084082+Zhxua@users.noreply.github.com>
2024-03-27 01:41:05 +0800

    title

--- display-not.php
+++ display-not.php
@@ -24,14 +24,14 @@
                 die("Connection failed: " . $con->connect_error);
             }
 
-            $sql = "SELECT id, subject, content, time FROM notification ORDER BY time DESC";
+            $sql = "SELECT id, title, content, time FROM notification ORDER BY time DESC";
             $result = $con->query($sql);
 
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     echo '<div class="notification">'; 
                     echo "<p><strong>DATE:</strong> " . $row["time"] . "</p>";
-                    echo "<p><strong>Title:</strong> " . htmlspecialchars($row["subject"]) . "</p>";
+                    echo "<p><strong>Title:</strong> " . htmlspecialchars($row["title"]) . "</p>";
                     echo "<p><strong>Content:</strong> " . $row["content"] . "</p>";
                     echo '</div>'; 
                 }
