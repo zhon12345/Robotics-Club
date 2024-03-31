4fbf27274d5dc406ac68ca3d20efafc876e8972a
Ng Zhun Onn <ngzhunonn+github@gmail.com>
2024-04-01 02:17:30 +0800

    user dashboard now displays username, adjusted dashboard spacing

--- user/dashboard.php
+++ user/dashboard.php
@@ -7,6 +7,7 @@ if (!isset($_SESSION['user'])) {
 }
 
 $title = 'User Dashboard';
+$user = $_SESSION['user'];
 
 include('../includes/header-user.php');
 ?>
@@ -14,11 +15,11 @@ include('../includes/header-user.php');
 <!-- content -->
 <section class="main-section">
     <div class="main-container">
-        <h2>Welcome <b><?php echo ADMIN_USER ?></b>, what would you like to do?</h2>
+        <h2>Welcome <b><?php echo $user ?></b>, what would you like to do?</h2>
 
         <div class="grid">
-            <a href="notification-display.php"><i class="fa-regular fa-envelope"></i>Notification</a>
-            <a href="user-information.php"><i class="fa-solid fa-user"></i>User Information</a>
+            <a href="notification-display.php"><i class="fa-regular fa-envelope"></i>View Notification</a>
+            <a href="user-information.php"><i class="fa-solid fa-user"></i>My Account</a>
         </div>
     </div>
 </section>
