9bfed4811b4d205741e455b5f2a9fd1685065081
YapSJin <yapsj-wg21@student.tarc.edu.my>
2024-03-31 22:07:39 +0800

    add

--- user/dashboard.php
+++ user/dashboard.php
@@ -7,24 +7,24 @@ if (!isset($_SESSION['user'])) {
 }
 
 $title = 'User Dashboard';
-include('../includes/header-admin.php');
+include('../includes/header-user.php');
 ?>
 <section class="main-section">
     <div class="main-container">
         <h2>Welcome <b></b>, what would you like to do?</h2>
 
-        include('../includes/header-user.php');
-        ?>
-
         <!-- content -->
         <section class="main-section">
             <div class="main-container">
-                <h2>Welcome <b><?php echo ADMIN_USER ?></b>, what would you like to do?</h2>
+                <h2>Welcome <b>User</b>, what would you like to do?</h2>
+                <div>
+                
+                </div>
 
                 <div class="grid">
                     <a href="notification-display.php"><i class="fa-regular fa-envelope"></i>Notification</a>
                     <a href="user-information.php"><i class="fa-solid fa-user"></i>User Information</a>
-                    <a href="tickit.php"><i class="fa-solid fa-ticket"></i>Tickit</a>
+                    <a href="ticket.php"><i class="fa-solid fa-ticket"></i>Ticket</a>
                 </div>
             </div>
         </section>
