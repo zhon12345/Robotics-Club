b99e112b0d13aa3fc56773cbe003bec59f4f2d19
Zhxua <163084082+Zhxua@users.noreply.github.com>
2024-03-31 20:53:43 +0800

    user-panel

--- includes/header-user.php
+++ includes/header-user.php
@@ -1,3 +1,5 @@
+<?php require_once('../includes/helper.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,8 +11,26 @@
 
     <!-- CSS Files -->
     <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/admin/admin.css">
     <link rel="stylesheet" href="<?php echo $css ?>" />
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
+<body>
+    <div class="sidenav">
+        <div class="container">
+            <div class="logo">
+                <a href="dashboard.php"><i class="fa-solid fa-user"></i><?php echo ADMIN_USER ?></a>
+            </div>
+            <div class="links">
+                <a href="notification-display.php">Notifications</a>
+                <a href="user-information.php">User Information</a>
+                <a href="tickit.php">Tickit</a>
+            </div>
+            <div class="logout">
+                <a href="../logout.php" class="logout"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
+            </div>
+        </div>
+    </div>
