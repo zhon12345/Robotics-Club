8ec068a495b9b1bec6a8b0a197ad296ab43b355c
YapSJin <yapsj-wg21@student.tarc.edu.my>
2024-03-31 20:20:04 +0800

    update user dashboard

--- user/dashboard.php
+++ user/dashboard.php
@@ -7,5 +7,22 @@ if (!isset($_SESSION['user'])) {
 }
 
 $title = 'User Dashboard';
-
+include('../includes/header-admin.php');
+?>
 include('../includes/header-user.php');
+<section class="main-section">
+    <div class="main-container">
+        <h2>Welcome <b></b>, what would you like to do?</h2>
+
+        <div class="grid">
+            <a href="events.php"><i class="fa-regular fa-calendar-days"></i>Event Tickets</a>
+            <a href="news.php"><i class="fa-solid fa-newspaper"></i> News</a>
+            <a href="users.php"><i class="fa-solid fa-users"></i>Edit User Information</a>
+            <a href="notification.php"><i class="fa-solid fa-bell"></i>Notification</a>
+        </div>
+    </div>
+</section>
+
+</body>
+
+</html>
